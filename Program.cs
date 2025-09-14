0433ca1997ba67f624cad5ce1e00bdcf213cf45d
Ds1ma <fisodhid745@gmail.com>
2025-09-14 21:18:49 +0300

    Delete Program.cs

--- Program.cs
+++
@@ -1,9 +0,0 @@
-﻿Console.WriteLine("введите R1");
-double R1 = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("введите R2");
-double R2 = Convert.ToDouble(Console.ReadLine());
-double P = Math.PI;
-double d = (P * Math.Pow(R2, 2));
-double d1 = (P * Math.Pow(R1, 2));
-double S = d - d1;
-Console.WriteLine(S);
